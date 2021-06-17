9cb2c151da5378b7ea00611218baa01d9ac6bc34
SvenTigges <sven.tigges@web.de>
2021-06-17 12:18:56 +0200

    ++ block 1 ++ hash

--- block.js
+++ block.js
@@ -1,6 +1,6 @@
 
 // Klassen Block
-class Block{
+class Block {
 
     // Konstruktor + Parameter 
     constructor(timestamp, lastHash, hash, data){
@@ -10,8 +10,25 @@ class Block{
         this.data = data;          // Data
     }
 
+    toString(){
+         return ` Block-
+            Timestamp: ${this.timestamp}
+            Last Hash: ${this.lastHash.substring(0,10)}
+            Hash:      ${this.hash.substring(0,10)}
+            data:      ${this.data}`;
+    }
 
+        static genesis() {
+            return new this("Genesis time", "-------", "f1r57-h45h", []);
+        }
 
+     static mineBlock(lastBlock, data) {  // Block 2 usw....
+        const timestamp = Date.now();     // Zeit im ms seit 01.01.1970 || how soon is now? 
+        const lastHash = lastBlock.hash;    
+        const hash = "to Du hash";
 
-
+        return new this(timestamp, lastHash, hash, data);
+     }
 }
+
+module.exports = Block; // Export als Modul 
