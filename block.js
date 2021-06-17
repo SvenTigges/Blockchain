17b56933d36d1555e4fe1e1a0116928c84698a2e
SvenTigges <sven.tigges@web.de>
2021-06-17 09:24:06 +0200

    ++ constructor

---
+++ block.js
@@ -0,0 +1,17 @@
+
+// Klassen Block
+class Block{
+
+    // Konstruktor + Parameter 
+    constructor(timestamp, lastHash, hash, data){
+        this.timestamp = timestamp;   // Zeitstemel
+        this.lastHash = lastHash;    // Hash des verheriger Block 
+        this.hash = hash;           // Eigener Hash
+        this.data = data;          // Data
+    }
+
+
+
+
+
+}
